12010aed3fb75588d3c887ead4d72b7935576fef
Steven Bock <steven.bockster@gmail.com>
2015-02-14 15:46:21 -0800

    Express scaffolding is working!

---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var express				= require('express'), //Express.js webserver
+	app_root			= __dirname, //placeholder for folder path
+	fs					= require('fs'), //Node.js filesystem library
+	morgan				= require('morgan'),	//HTTP request console logger
+	errorhandler		= require('errorHandler'), //error logger
+	mongoose			= require('mongoose'), //MongoDB DBMS driver
+	bodyParser			= require('body-parser'); //easy reading of JSON encoding
+
+//initialize Express server
+var app = express();
+
+//JSON dependency
+app.use(bodyParser.json());
+
+//log EVERY HTTP request to the console
+app.use(morgan('dev'));
+
+//log ALL build/runtime errors
+app.use(errorhandler( { dumpExceptions: true, showStack: true } ) );
+
+//HTTP request to index.html
+app.get('/', function(req, res){
+	//send index.html on GET request
+	res.sendFile(app_root + '/client/index.html');
+});
+
+//redirect HTTP requests to correct directories
+app.use('/js', express.static(app_root + '/client/js'));
+app.use('/css', express.static(app_root + '/client/css'));
+
+//listener - param is port number
+app.listen(3000, function(){
+	console.log('Express.js server now listening on port 3000...');
+});
